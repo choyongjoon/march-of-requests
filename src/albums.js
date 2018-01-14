37c820519c0396d48b2b6be7a0ea4dfaff55d836
Cho, Yongjoon <w2000wkd@gmail.com>
2018-01-14 15:12:42 +0900

    Add all albums and songs

--- src/albums.js
+++ src/albums.js
@@ -3,6 +3,7 @@ const albums = [
     id: 13,
     title: '서교',
     date: new Date('2017-10-24'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/002/250/2250313.jpg?type=r204Fll&v=20171026172903',
     tracks: [
       {
         title: '미래(未來)'
@@ -40,6 +41,7 @@ const albums = [
     id: 12,
     title: '인디 달링을 찾아서',
     date: new Date('2014-04-03'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/422/422573.jpg?type=r204Fll&v=20150722101906',
     tracks: [
       {
         title: '인디 달링'
@@ -73,6 +75,326 @@ const albums = [
       },
       {
         title: '둘'
+      }
+    ]
+  },
+  {
+    id: 11,
+    title: '정월의 루미나리에',
+    date: new Date('2013-12-26'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/409/409746.jpg?type=r204Fll&v=20150722101605',
+    tracks: [
+      {
+        title: '정월의 루미나리에'
+      }
+    ]
+  },
+  {
+    id: 10,
+    title: '영원의 단면',
+    date: new Date('2013-05-14'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/378/378740.jpg?type=r204Fll&v=20151007114306',
+    tracks: [
+      {
+        title: '안아줘'
+      },
+      {
+        title: '영원의 단면'
+      },
+      {
+        title: '그랑프리'
+      },
+      {
+        title: '가장 최근의 꿈'
+      },
+      {
+        title: '여름과 꿈과 밤의 모든 매력'
+      },
+      {
+        title: '눈사람들이 떠나고 나면'
+      },
+      {
+        title: '배드민턴'
+      },
+      {
+        title: '88년의 여름'
+      },
+      {
+        title: '회전목마의 밤'
+      },
+      {
+        title: '마지막 담배'
+      },
+      {
+        title: '빗방울보'
+      },
+      {
+        title: '너의 손글씨'
+      },
+      {
+        title: '체리 블라썸 (Bonus Track)'
+      }
+    ]
+  },
+  {
+    id: 9,
+    title: '체리 블라썸',
+    date: new Date('2013-04-04'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/374/374213.jpg?type=r204Fll&v=20151007114306',
+    tracks: [
+      {
+        title: '체리 블라썸'
+      }
+    ]
+  },
+  {
+    id: 8,
+    title: 'B (EP)',
+    date: new Date('2010-03-25'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/170/170833.jpg?type=r204Fll&v=20151006163103',
+    tracks: [
+      {
+        title: '하얀 마법 속삭임'
+      },
+      {
+        title: 'Favorite'
+      },
+      {
+        title: '돌아와'
+      },
+      {
+        title: 'Plan B'
+      },
+      {
+        title: '나의 목소리'
+      },
+      {
+        title: '시모네타'
+      },
+      {
+        title: '한국소녀의 겨울'
+      }
+    ]
+  },
+  {
+    id: 7,
+    title: '돌아와 / 한국소녀의 겨울 (Single)',
+    date: new Date('2009-09-17'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/162/162328.jpg?type=r204Fll&v=20150813180914',
+    tracks: [
+      {
+        title: '돌아와'
+      },
+      {
+        title: '한국소녀의 겨울'
+      }
+    ]
+  },
+  {
+    id: 6,
+    title: 'Hot Music',
+    date: new Date('2007-10-01'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/061/61845.jpg?type=r204Fll&v=20141203120910',
+    tracks: [
+      {
+        title: '소나기의 첫 번째 물방울'
+      },
+      {
+        title: 'Baby Baby Baby Baby Baby'
+      },
+      {
+        title: '펭귄을 기른다는 것'
+      },
+      {
+        title: 'JH Loves You (Feat. Naeun of I Love JH)'
+      },
+      {
+        title: '여자옷'
+      },
+      {
+        title: 'Disney Girl'
+      },
+      {
+        title: '모든 스텝을 아는 소녀'
+      },
+      {
+        title: '간접 키스'
+      },
+      {
+        title: '천사들의 오후'
+      },
+      {
+        title: '실용 스페인어'
+      },
+      {
+        title: '넘쳐나는 인생'
+      },
+      {
+        title: 'Disney Girl (Japanese Ver.)'
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: '당신은 울기 위해 태어난 사람',
+    date: new Date('2006-07-07'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/040/40475.jpg?type=r204Fll&v=20141203120006',
+    tracks: [
+      {
+        title: '봄의 첫 날'
+      },
+      {
+        title: '폭포'
+      },
+      {
+        title: '당신은 울기 위해 태어난 사람'
+      },
+      {
+        title: '이방인의 재'
+      },
+      {
+        title: '사계절이여 안녕'
+      },
+      {
+        title: '세상에 없는 마음'
+      },
+      {
+        title: '기도'
+      },
+      {
+        title: '낯선 사람'
+      },
+      {
+        title: '안녕, 미안, 안녕'
+      },
+      {
+        title: '잊혀지기 쉬운'
+      },
+      {
+        title: '빠른 구름'
+      },
+      {
+        title: '한겨울의 천둥처럼'
+      },
+      {
+        title: '기도'
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: '빛방울보들',
+    date: new Date('2005-06-30'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/033/33235.jpg?type=r204Fll&v=20111124154547',
+    tracks: [
+      {
+        title: '빛방울보 (edit ver.)'
+      },
+      {
+        title: '여자들의 이별'
+      },
+      {
+        title: '빛방울보 (acoustic ver.)'
+      },
+      {
+        title: '꿈열흘밤 (Live)'
+      },
+      {
+        title: '빛방울보 (live)'
+      },
+      {
+        title: '가끔 미치도록 네가 안고 싶어질 때가 있어'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: '영원의 단면',
+    date: new Date('2005-04-04'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/032/32022.jpg?type=r204Fll&v=20130515170019',
+    tracks: [
+      {
+        title: '안아줘'
+      },
+      {
+        title: '영원의 단면'
+      },
+      {
+        title: '2110'
+      },
+      {
+        title: '가장 최근의 꿈'
+      },
+      {
+        title: '여름과 꿈과 밤의 모든 매력'
+      },
+      {
+        title: '눈사람들이 떠나고 나면'
+      },
+      {
+        title: '배드민턴'
+      },
+      {
+        title: '88년의 여름'
+      },
+      {
+        title: '회전목마의 밤'
+      },
+      {
+        title: '마지막 담배'
+      },
+      {
+        title: '빗방울보'
+      },
+      {
+        title: '너의 손글씨'
+      },
+      {
+        title: '(Hidden Track)'
+      }
+    ]
+  },
+  {
+    id: 1,
+    title: '가벼운 숨결',
+    date: new Date('2001-01-07'),
+    coverUrl: 'http://musicmeta.phinf.naver.net/album/000/015/15417.jpg?type=r204Fll&v=20170706153004',
+    tracks: [
+      {
+        title: '오르골'
+      },
+      {
+        title: '문학 선생님'
+      },
+      {
+        title: 'Corazon'
+      },
+      {
+        title: '꿈열흘밤'
+      },
+      {
+        title: '유성우'
+      },
+      {
+        title: 'Aishiteru'
+      },
+      {
+        title: '답장'
+      },
+      {
+        title: '동감'
+      },
+      {
+        title: 'Singalong'
+      },
+      {
+        title: 'Need A Woman, Not A Girl'
+      },
+      {
+        title: '카드세실'
+      },
+      {
+        title: '연날리던 손'
       }
     ]
   }
