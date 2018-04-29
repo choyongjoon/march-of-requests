8a2b221bca3e2b036916d07832a2d9a455d7b9bb
Cho, Yongjoon <w2000wkd@gmail.com>
2018-04-29 22:14:53 +0900

    Add 2018 tracks

--- src/orders2018.js
+++ src/orders2018.js
@@ -58,6 +58,171 @@ const orders2018 = [
   },
   {
     title: '펭귄을 기른다는 것'
+  },
+  {
+    title: 'Ariel'
+  },
+  {
+    title: '둘'
+  },
+  {
+    title: '꿈열흘밤'
+  },
+  {
+    title: '이방인의 재'
+  },
+  {
+    title: '배드민턴'
+  },
+  {
+    title: 'Plan B'
+  },
+  {
+    title: '안아줘'
+  },
+  {
+    title: '세러네이드'
+  },
+  {
+    title: '너의 손글씨'
+  },
+  {
+    title: '그랑프리 / 2110'
+  },
+  {
+    title: '안경전쟁'
+  },
+  {
+    title: 'Singalong'
+  },
+  {
+    title: '체리 블라썸 / 활짝 핀 벚꽃나무 아래서'
+  },
+  {
+    title: '오르골'
+  },
+  {
+    title: '돌아와'
+  },
+  {
+    title: 'Corazon'
+  },
+  {
+    title: '안녕, 미안, 안녕'
+  },
+  {
+    title: 'Disney Girl'
+  },
+  {
+    title: '처형 직전의 도스토옙스키'
+  },
+  {
+    title: '세상에 없는 마음'
+  },
+  {
+    title: '모든 스텝을 아는 소녀'
+  },
+  {
+    title: '폭포'
+  },
+  {
+    title: '여름과 꿈과 밤의 모든 매력'
+  },
+  {
+    title: 'Favorite'
+  },
+  {
+    title: '순환선'
+  },
+  {
+    title: '간접 키스'
+  },
+  {
+    title: '눈사람들이 떠나고 나면'
+  },
+  {
+    title: '빠른 구름'
+  },
+  {
+    title: 'Baby Baby Baby Baby Baby'
+  },
+  {
+    title: '88년의 여름'
+  },
+  {
+    title: '카드세실'
+  },
+  {
+    title: 'Need A Woman, Not A Girl'
+  },
+  {
+    title: '당신은 울기 위해 태어난 사람'
+  },
+  {
+    title: 'JH Loves You'
+  },
+  {
+    title: '사계절이여 안녕'
+  },
+  {
+    title: '한국소녀의 겨울'
+  },
+  {
+    title: '벼락'
+  },
+  {
+    title: '천사들의 오후'
+  },
+  {
+    title: '하얀 마법 속삭임'
+  },
+  {
+    title: '기도'
+  },
+  {
+    title: 'Miss Chocolate'
+  },
+  {
+    title: '유성우'
+  },
+  {
+    title: '답장'
+  },
+  {
+    title: '차를 댈 곳'
+  },
+  {
+    title: '아침노을'
+  },
+  {
+    title: '여자옷'
+  },
+  {
+    title: '가벼운 숨결'
+  },
+  {
+    title: 'Air Hawaii'
+  },
+  {
+    title: '봄의 첫 날'
+  },
+  {
+    title: '잊혀지기 쉬운'
+  },
+  {
+    title: 'Kiss (세상의 모든 해변)'
+  },
+  {
+    title: '실용 스페인어'
+  },
+  {
+    title: '한겨울의 천둥처럼'
+  },
+  {
+    title: '낯선 사람'
+  },
+  {
+    title: '연날리던 손'
   }
 ]
 
